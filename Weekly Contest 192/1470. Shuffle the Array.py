1e28718710dac6da76a2911ada1ad5c52663d3a8
dxl2016 <60236777+dxl2016@users.noreply.github.com>
2020-06-07 11:33:52 -0400

    Create 1470. Shuffle the Array.py

---
+++ Weekly Contest 192/1470. Shuffle the Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        if not nums:
+            return
+        
+        ans = []
+        i = 0
+        while(i<n):
+            ans.append(nums[i])
+            ans.append(nums[i+n])
+            i += 1
+        return ans
