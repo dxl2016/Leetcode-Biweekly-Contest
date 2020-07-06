96f51935cc22f1324c7542738e6e8389b89748be
dxl2016 <60236777+dxl2016@users.noreply.github.com>
2020-07-05 21:08:40 -0400

    Create 1504. Count Submatrices With All Ones.py

---
+++ Weekly Contest 196/1504. Count Submatrices With All Ones.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        if not mat:
+            return 0
+        
+        m, n = len(mat), len(mat[0])
+        count = 0
+        for left in range(n):
+            row_sums = [0 for _ in range(m)]
+            for right in range(left, n):
+                consec_count = 0
+                width = right - left + 1
+                for bottom in range(m):
+                    row_sums[bottom] += mat[bottom][right]
+                    if row_sums[bottom] != width:
+                        consec_count = 0
+                    else:
+                        consec_count += 1
+                        count += consec_count
+        return count
+
