6df17576d38936feae7d10407ff55fd6c2c863c9
dxl2016 <60236777+dxl2016@users.noreply.github.com>
2020-08-19 11:05:40 -0400

    Create 1552. Magnetic Force Between Two Balls.py

---
+++ Weekly Contest 202/1552. Magnetic Force Between Two Balls.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        position = sorted(position)
+        n = len(position)
+        if n == 2:
+            return position[-1]-position[0]
+        
+        cur_min = 1
+        cur_max = position[-1]-position[0]+1
+        
+        def enough(distance):
+            ini = position[0]
+            c = 1
+            for i in range(1, n):
+                if position[i]-ini >= distance:
+                    ini = position[i]
+                    c += 1
+                
+            return c>=m
+            
+        while(cur_max-cur_min>1):
+            mid = (cur_max+cur_min)//2
+            if enough(mid):
+                cur_min = mid
+            else:
+                cur_max = mid
+        
+        return cur_min
+
